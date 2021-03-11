07188d8d17c2491918a645d4881efce2c2fc3841
Jaspaul Bola <jaspaul.b@gmail.com>
2021-03-11 00:28:44 -0500

    Re-enable dialog tests

--- packages/botkit/tests/Dialog.tests.js
+++ packages/botkit/tests/Dialog.tests.js
@@ -12,7 +12,7 @@ describe('Botkit dialog', function() {
         });
     });
 
-    xit('should follow a dialog', async function() {
+    it('should follow a dialog', async function() {
         const introDialog = new BotkitConversation('introduction', bot);
         introDialog.ask({
             text: 'You can say Ok',
@@ -62,7 +62,7 @@ describe('Botkit dialog', function() {
         assert(reply6.text === 'you said 2', 'wrong reply 6');
     });
 
-    xit('should work with convo.repeat', async function() {
+    it('should work with convo.repeat', async function() {
         const botConvo = new BotkitConversation('testConvo', bot);
 
         botConvo.ask({
